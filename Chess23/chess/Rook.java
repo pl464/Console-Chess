b5937b503b999b3292e5535be7dd247fd1fd067b
Lance Luo <lance.luo@rutgers.edu>
2020-03-22 00:22:55 +0000

    added Rook javadoc

--- Chess23/chess/Rook.java
+++ Chess23/chess/Rook.java
@@ -1,16 +1,38 @@
 package chess;
 
+import java.util.*;
+import java.io.*;
+
 /**
-@author Lance Luo
-@author Patrick Lee
+* @author Lance Luo
+* @author Patrick Lee
+* Class to represent a rook piece.
 */
 public class Rook extends Piece {
+	/**
+	* Boolean to track whether this rook has been moved yet.
+	*/
 	public boolean canCastle = true;
 
+	/**
+	* Constructor for a rook piece.
+	* @param color Sets the piece as white or black.
+	* @param type Sets the piece as a rook.
+	*/
 	Rook (char color) {
 		super(color, 'R');
 	}
-	
+
+	/**
+	* Method to determine whether a move is valid for a rook.
+	* @param board 2D array that represents the chessboard.
+	* @param startCol Column where the piece starts.
+	* @param startRow Row where the piece starts.
+	* @param endCol Column where the piece ends.
+	* @param endRow Row where the piece ends.
+	* @param turn Odd number means white's turn, even number means black's turn.
+	* @return Returns true if the move is valid, false if the move is invalid.
+	*/
 	public boolean validMove(Piece[][] board, int startCol, int startRow, int endCol, int endRow, int turn) {
 		int xDist = endCol - startCol;
 		int yDist = endRow - startRow;
