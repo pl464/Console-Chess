58ec0f5dbcf67568071fc652e137c2916209a5b2
Lance Luo <lance.luo@rutgers.edu>
2020-03-22 00:16:49 +0000

    added Knight javadoc

--- Chess23/chess/Knight.java
+++ Chess23/chess/Knight.java
@@ -1,14 +1,33 @@
 package chess;
 
+import java.util.*;
+import java.io.*;
+
 /**
-@author Lance Luo
-@author Patrick Lee
+* @author Lance Luo
+* @author Patrick Lee
+* Class to represent a knight piece.
 */
 public class Knight extends Piece {
+	/**
+	* Constructor for a knight piece.
+	* @param color Sets the piece as white or black.
+	* @param type Sets the piece as a knight.
+	*/
 	Knight (char color) {
 		super(color, 'N');
 	}
-	
+
+	/**
+	* Method to determine whether a move is valid for a knight.
+	* @param board 2D array that represents the chessboard.
+	* @param startCol Column where the piece starts.
+	* @param startRow Row where the piece starts.
+	* @param endCol Column where the piece ends.
+	* @param endRow Row where the piece ends.
+	* @param turn Odd number means white's turn, even number means black's turn.
+	* @return Returns true if the move is valid, false if the move is invalid.
+	*/
 	public boolean validMove(Piece[][] board, int startCol, int startRow, int endCol, int endRow, int turn) {
 		int xDist = endCol - startCol;
 		int yDist = endRow - startRow;
